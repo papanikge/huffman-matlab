9f583634e9de5f70506fa398b587fb3ce105c093
George 'papanikge' Papanikolaou <g3orge.app@gmail.com>
2015-12-07 18:30:51 +0200

    new file for benchs for the third question

---
+++ bench_sources2.m
@@ -0,0 +1,27 @@
+function bench_sources2()
+% Third requirement of the first assignment.
+% Testing source B with a dict based on probabilities from the file.
+% George 'papanikge' Papanikolaou CEID 2015
+
+fprintf('Generating the dict based on the file...\n');
+[chars probs] = estimate_probs('kwords.txt', 1)
+dict = huffmandict(chars, probs);
+
+fprintf('Pull the file in...\n');
+B = sourceB();
+fprintf('Encoding and decoding back to back ...\n');
+tic;
+for i=1:length(B)
+    c = strtrim(B(i,:));
+    % Getting a cell of 'correct' characters. Still one-by-one.
+    c = mat2cell(c, ones(1,1), ones(1,length(c)));
+    % The actual thing.
+    c_enc = huffmanenco(c, dict);
+    c_dec = huffmandeco(c_enc, dict);
+    % Check whether the decoding is correct.
+    if ~isequal(c, c_dec)
+        error('Error! Encoded and decoded not matching.')
+    end
+end
+fprintf('Tests passed successfully!!!\n');
+toc
