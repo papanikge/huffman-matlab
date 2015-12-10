5d99b0f0c346ea1cc38be6f8103a107af4568544
George 'papanikge' Papanikolaou <g3orge.app@gmail.com>
2015-12-11 01:44:43 +0200

    implement the first section of the fifth req

--- bench_sourceB_2char.m
+++ bench_sourceB_2char.m
@@ -3,13 +3,52 @@ function bench_sourceB_2char()
 % Testing source B with a 2-char dict based on the file and more...
 % George 'papanikge' Papanikolaou CEID 2015
 
+fprintf('>> First: Checking against computed probabilities.\n');
 fprintf('Generating the dict...\n');
-% TODO
+[eng_couples, eng_couple_probs] = generate_probs(2);
+[dict, avg] = myhuffmandict(eng_couples, eng_couple_probs);
+fprintf('Average Huffman code length is %f bits.\n', avg);
 
-fprintf('Generate source...\n');
-
+fprintf('Pulling the file in...\n');
+B = sourceB();
 fprintf('Encoding and decoding back to back ...\n');
 tic;
-% TODO
+for i=1:length(B)
+    % `c` is a word of the text. We're gonna need to skip words that we don't
+    % have symbols for. (or chars that are not couples)
+    c = strtrim(B(i,:));
+    c = lower(c);
+    % For some god forsaken reason I can't chain clauses.
+    if length(c) == 1
+        continue;
+    end
+    if strfind(c, '-')
+        continue;
+    end
+    if strfind(c, '.')
+        continue;
+    end
+    if strfind(c, '/')
+        continue;
+    end
+    if strfind(c, char(39))
+        continue;
+    end
+    % We're deleting extra 'stray' characters, because we just have the eng-alphabet pairs.
+    if mod(length(c), 2) ~= 0
+        c(length(c)) = [];
+    end
+    % Splitting to two by two chars cell.
+    c = mat2cell(c, ones(1,1), ones(1,length(c)/2)*2);
+    % The actual thing.
+    c_enc = myhuffmanenco(c, dict);
+    c_dec = myhuffmandeco(c_enc, dict);
+    % Check whether the decoding is correct.
+    if ~isequal(c, c_dec)
+        c
+        c_dec
+        error('Error! Encoded and decoded not matching.')
+    end
+end
 fprintf('Tests passed successfully!!!\n');
 toc
