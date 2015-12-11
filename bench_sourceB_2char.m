e7cba2b6d7139dbc5221b5136e81fae3f822fe25
George 'papanikge' Papanikolaou <g3orge.app@gmail.com>
2015-12-11 14:46:19 +0200

    add file section for the second req

--- bench_sourceB_2char.m
+++ bench_sourceB_2char.m
@@ -52,3 +52,37 @@ for i=1:length(B)
 end
 fprintf('Tests passed successfully!!!\n');
 toc
+
+fprintf('>> Second: Checking against probabilities based on the file itself.\n');
+fprintf('Generating the dict...\n');
+[eng_couples eng_couple_probs] = estimate_probs('kwords.txt', 2);
+[dict, avg] = myhuffmandict(eng_couples, eng_couple_probs);
+fprintf('Average Huffman code length is %f bits.\n', avg);
+
+fprintf('Pulling the file in...\n');
+B = sourceB();
+fprintf('Encoding and decoding back to back ...\n');
+tic;
+for i=1:length(B)
+    % `c` is a word of the text. We're gonna need to skip words that we don't
+    % have symbols for. (or chars that are not couples)
+    c = strtrim(B(i,:));
+    c = lower(c);
+    % We still don't want
+    if length(c) == 1
+        continue;
+    end
+    % Splitting to two by two chars cell.
+    c = mat2cell(c, ones(1,1), ones(1,length(c)/2)*2);
+    % The actual thing.
+    c_enc = myhuffmanenco(c, dict);
+    c_dec = myhuffmandeco(c_enc, dict);
+    % Check whether the decoding is correct.
+    if ~isequal(c, c_dec)
+        c
+        c_dec
+        error('Error! Encoded and decoded not matching.')
+    end
+end
+fprintf('Tests passed successfully!!!\n');
+toc
