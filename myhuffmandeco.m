2dad38661a8cf0ab7d02d5110091ab37267a54c1
George 'papanikge' Papanikolaou <g3orge.app@gmail.com>
2015-12-11 19:13:23 +0200

    reimplement myhuffmandeco using regex

--- myhuffmandeco.m
+++ myhuffmandeco.m
@@ -2,25 +2,38 @@ function text = myhuffmandeco(enco, dict)
 %
 % HUFFMANDECO function file
 % Decodes the incoming 'enco' arg based on a huffman code using the 'dict' arg
+% Second version with regexp. Slower but better.
 %
 
 %   Copyright 2015 George 'papanikge' Papanikolaou
-%   $Revision: 1.0 $  $Date: 2015/12/01 03:12:20 $
+%   $Revision: 2.0 $  $Date: 2015/12/11 15:12:20 $
 
+% New algorithm. We're gonna work with regular expressions.
 i = 1;
-while i<=length(enco)
-    % We are not working char-wise for the decoding
-    % looking through the dictionary
-    for j=1:length(dict)
-        l = length(dict{j,2});          % `l` is the length of the current code
-        if enco(i:(i+(l-1))) == dict{j,2}
-            try
-                text = [text {dict{j,1}}];
-            catch
-                text = {dict{j,1}};
-            end
-            break;
+enco = sprintf('%d', enco);
+while i <= length(dict)
+    % Convert them to strings.
+    code = sprintf('%d', dict{i,2});
+    % Make the pattern. '^' is the string beginning anchor, we need the pattern
+    % on the front to make sure it's the correct code (see: prefix-code).
+    patt = ['^' code];
+    if regexp(enco, patt);
+        try
+            text = [text {dict{i,1}}];
+        catch
+            text = {dict{i,1}};
         end
+        % Let's go from the beginning...
+        i = 1;
+        % Updating the encoding part for the next iteration.
+        le = length(enco);
+        lc = length(code) + 1;
+        if lc > le
+            break;
+        else
+            enco = enco(lc:le);
+        end
+    else
+        i = i + 1;
     end
-    i = i + l;
 end
